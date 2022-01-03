ca5455e77467a356da5f4b76fa3bb46936b9d20f
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-01-03 23:07:34 +0530

    Files added

---
+++ android/app/src/main/kotlin/dev/dileepabandara/flutter_with_payhere/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.dileepabandara.flutter_with_payhere
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter with PayHere',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter with PayHere'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter with PayHere'),
+      ),
+      body: const Center(
+        child: Text("Welcome"),
+      ),
+    );
+  }
+}
